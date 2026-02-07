fe4d573ef6838240019b3ed398451b8b59363eec
LAB 206 <lab206@SAKEC.LOCAL>
2026-02-07 13:48:41 +0530

    Updated backend API in apis.js

--- apis/api.js
+++ apis/api.js
@@ -2,12 +2,12 @@
 import { Platform } from "react-native";
 
 // ✅ Correct Syntax: Use a ternary operator
-export const host = Platform.OS === "android" 
-  ? "http://10.0.2.2:3000" 
-  : "http://localhost:3000";
+// export const host = Platform.OS === "android" 
+//   ? "http://10.0.2.2:3000" 
+//   : "http://localhost:3000";
 //export const host = "http://10.0.2.2:3000"; //Production
 // export const host = "https://ngo-attendance-app-server.onrender.com"; //Production
-// export const host = "https://ngo-attendance-backend.el.r.appspot.com"; //Production
+export const host = "https://ngo-attendance-backend.el.r.appspot.com"; //Production
 
 //login auth apis
 export const auth_host = `${host}/api/v1/auth`;
